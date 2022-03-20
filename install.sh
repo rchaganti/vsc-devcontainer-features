3b2289c77ac6e1917132a382c000eae7841d07b1
Ravikanth C <ravikanth@ravichaganti.com>
2022-03-20 21:03:46 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,11 +10,17 @@ if [ ! -z ${_BUILD_ARG_AZUREBICEP} ]; then
     echo "Activating feature 'Azure Bicep'"
 
     # Build args are exposed to this entire feature set following the pattern:  _BUILD_ARG_<FEATURE ID>_<OPTION NAME>
-    VERSION=${_BUILD_ARG_AZUREBICEP_VERSION:-undefined}
+    VERSION=${_BUILD_ARG_AZUREBICEP_VERSION:-"latest"}
+
+    if [ "${VERSION}" = "latest" ]; then        
+        URL="https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64"
+    else
+        URL="https://github.com/Azure/bicep/releases/download/${VERSION}/bicep-linux-x64"
+    fi
 
     sudo tee /usr/installbicep.sh > /dev/null \
     << EOF
-    sudo curl -Lo /usr/local/bin/bicep https://github.com/Azure/bicep/releases/download/${VERSION}/bicep-linux-x64
+    sudo curl -Lo /usr/local/bin/bicep ${URL}
     sudo chmod +x /usr/local/bin/bicep
 EOF
     sudo chmod +x /usr/installbicep.sh
