af63d18466ed8b133b6c600dc3d101c1183ad525
Ravikanth C <ravikanth@ravichaganti.com>
2022-03-20 21:52:50 +0530

    adding extension version installs

--- install.sh
+++ install.sh
@@ -9,19 +9,31 @@ set +a
 if [ ! -z ${_BUILD_ARG_AZUREBICEP} ]; then
     echo "Activating feature 'Azure Bicep'"
 
-    # Build args are exposed to this entire feature set following the pattern:  _BUILD_ARG_<FEATURE ID>_<OPTION NAME>
-    VERSION=${_BUILD_ARG_AZUREBICEP_VERSION:-"latest"}
+    # Azure Bicep CLI version 
+    CLIVERSION=${_BUILD_ARG_AZUREBICEP_CLI:-"latest"}
 
-    if [ "${VERSION}" = "latest" ]; then        
-        URL="https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64"
+    if [ "${CLIVERSION}" = "latest" ]; then        
+        CLIURL="https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64"
     else
-        URL="https://github.com/Azure/bicep/releases/download/${VERSION}/bicep-linux-x64"
+        CLIURL="https://github.com/Azure/bicep/releases/download/${CLIVERSION}/bicep-linux-x64"
+    fi
+
+    # Azure Bicep VS Code extension version 
+    EXTNVERSION=${_BUILD_ARG_AZUREBICEP_EXTENSION:-"latest"}
+
+    if [ "${EXTNVERSION}" = "latest" ]; then        
+        EXTNURL="https://github.com/Azure/bicep/releases/latest/download/vscode-bicep.vsix"
+    else
+        EXTNURL="https://github.com/Azure/bicep/releases/download/${EXTNURL}/vscode-bicep.vsix"
     fi
 
     sudo tee /usr/installbicep.sh > /dev/null \
     << EOF
-    sudo curl -Lo /usr/local/bin/bicep ${URL}
+    sudo curl -Lo /usr/local/bin/bicep ${CLIURL}
     sudo chmod +x /usr/local/bin/bicep
+
+    sudo curl -Lo /tmp/vscode-bicep.vsix ${EXTNURL}
+    sudo code --install-extension /tmp/vscode-bicep.vsix
 EOF
     sudo chmod +x /usr/installbicep.sh
 fi
