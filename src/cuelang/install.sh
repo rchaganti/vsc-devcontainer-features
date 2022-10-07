5f3c3e0f1b19a00b3607625523004565a09d136a
Ravikanth C <ravikanth@ravichaganti.com>
2022-10-07 20:06:43 +0530

    fixes to install scripts

--- src/cuelang/install.sh
+++ src/cuelang/install.sh
@@ -13,6 +13,8 @@ VERSION=${VERSION:-"latest"}
 CUEURL="https://github.com/cue-lang/cue/releases/download/v${VERSION}/cue_v${VERSION}_linux_amd64.tar.gz"
 
 sudo curl -Lo /tmp/cue.tar.gz ${CUEURL}
+sudo mkdir /tmp/cue
 sudo tar xvzf /tmp/cue.tar.gz -C /tmp/cue
 sudo cp /tmp/cue/cue /usr/local/bin/cue 
 sudo chmod +x /usr/local/bin/cue
+sudo rm -rf /tmp/cue
