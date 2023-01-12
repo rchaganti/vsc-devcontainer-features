bd6b2180e6a72134abaf9274bc44584c9b0ff0cf
Ravikanth C <ravikanth@ravichaganti.com>
2023-01-12 15:35:20 +0530

    fixing install

--- src/wabt/install.sh
+++ src/wabt/install.sh
@@ -5,8 +5,8 @@ echo "Activating feature 'The WebAssembly Binary Toolkit'"
 
 # Get cmake
 cd ~
-sudo curl -Lo ~/cmake.sh https://github.com/Kitware/CMake/releases/download/v3.25.1/cmake-3.25.1-linux-x86_64.sh
-sudo chmod +x ~/cmake.sh
+curl -Lo ~/cmake.sh https://github.com/Kitware/CMake/releases/download/v3.25.1/cmake-3.25.1-linux-x86_64.sh
+chmod +x ~/cmake.sh
 
 sudo mkdir /usr/local/bin/cmake
 ./cmake.sh --skip-license --prefix=/usr/local/bin/cmake
@@ -14,20 +14,20 @@ export PATH="${PATH}:/usr/local/bin/cmake/bin"
 rm ~/cmake.sh
 
 # Clone and build wabt
-sudo git clone https://github.com/WebAssembly/wabt.git
-sudo cd wabt
-sudo git submodule update --init
+git clone https://github.com/WebAssembly/wabt.git
+cd wabt
+git submodule update --init
 
-sudo mkdir build
-sudo cd build
-sudo cmake ..
-sudo cmake --build .
+mkdir build
+cd build
+cmake ..
+cmake --build .
 
-sudo cd ..
+cd ..
 sudo mkdir /usr/local/bin/wabt
 sudo mv build/ /usr/local/bin/wabt/bin
 
-sudo cd ..
-sudo rm -rf wabt/
+cd ..
+rm -rf wabt/
 
 export PATH="${PATH}:/usr/local/bin/wabt/bin"
