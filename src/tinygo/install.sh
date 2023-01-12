f85c72f4a5dcc2e4bbb4f91ae1a9f42977efa5e5
Ravikanth C <ravikanth@ravichaganti.com>
2023-01-12 10:12:26 +0530

    tinygo

---
+++ src/tinygo/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Activating feature 'Tiny Go'"
+
+# Azure Bicep CLI version 
+VERSION=${VERSION:-"latest"}
+ARCHITECTURE="amd64"
+
+if [ "$(dpkg --print-architecture)" ==  "arm64" ]; then
+    ARCHITECTURE="arm64"
+fi
+
+TINYURL="https://github.com/tinygo-org/tinygo/releases/download/v${VERSION}/tinygo_${VERSION}_${ARCHITECTURE}.deb"
+
+sudo curl -Lo /usr/local/bin/bicep ${TINYURL}
+sudo dpkg -i tinygo_${VERSION}_{ARCHITECTURE}.deb
+
+export PATH=$PATH:/usr/local/bin
